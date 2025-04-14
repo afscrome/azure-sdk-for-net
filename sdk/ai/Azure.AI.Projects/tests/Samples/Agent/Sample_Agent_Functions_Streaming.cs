e5b493b0fa8876b2f2ede3a2a1bd41d2b002c32a
howieleung <howieleung@microsoft.com>
2025-04-14 16:38:43 -0700

    Moved tests

--- sdk/ai/Azure.AI.Projects/tests/Samples/Agent/Sample_Agent_Functions_Streaming.cs
+++ sdk/ai/Azure.AI.Projects/tests/Samples/Agent/Sample_Agent_Functions_Streaming.cs
@@ -15,6 +15,232 @@ namespace Azure.AI.Projects.Tests;
 
 public partial class Sample_Agent_Functions_Streaming : SamplesBase<AIProjectsTestEnvironment>
 {
+    private class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+
+    private int GetHumidityByAddress(Address address)
+    {
+        return (address.City == "Seattle") ? 60 : 80;
+    }
+
+    private FunctionToolDefinition geHhumidityByAddressTool = new(
+         name: "GetHumidityByAddress",
+         description: "Get humidity by street and city",
+         parameters: BinaryData.FromObjectAsJson(
+         new
+         {
+             Type = "object",
+             Properties = new
+             {
+                 Address = new
+                 {
+                     Type = "object",
+                     Properties = new
+                     {
+                         Street = new
+                         {
+                             Type = "string",
+                             Description = "Street"
+                         },
+                         City = new
+                         {
+                             Type = "string",
+                             Description = "city"
+                         },
+                     },
+                     Required = new[] { "street", "city" }
+                 }
+             },
+             Required = new[] { "address" }
+         },
+         new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }));
+    private string[] GetWeatherByAddresses(Dictionary<string, string>[] addresses, string unit = "F")
+    {
+        string[] temps = new string[addresses.Length];
+        for (int i = 0; i < addresses.Length; i++)
+        {
+            if (addresses[i].TryGetValue("city", out string city))
+            {
+                temps[i] = string.Format("{0}{1}", (city == "Seattle") ? "20" : "50", unit);
+            }
+            else
+            {
+                throw new ArgumentException("Each address must contain 'street' and 'city' keys.");
+            }
+        }
+        return temps;
+    }
+    private FunctionToolDefinition getWeatherByAddressesTool = new(
+         name: "GetWeatherByAddresses",
+         description: "Get weather by street and city",
+         parameters: BinaryData.FromObjectAsJson(
+         new
+         {
+             Type = "object",
+             Properties = new
+             {
+                 Addresses = new
+                 {
+                     Type = "array",
+                     Description = "A list of addresses",
+                     Items = new
+                     {
+                         Type = "object",
+                         Properties = new
+                         {
+                             Street = new
+                             {
+                                 Type = "string",
+                                 Description = "Street"
+                             },
+                             City = new
+                             {
+                                 Type = "string",
+                                 description = "city"
+                             },
+                         },
+                         Required = new[] { "street", "city" }
+                     }
+                 },
+                 Unit = new
+                 {
+                     Type = "string",
+                     Enum = new[] { "c", "f" },
+                 },
+             },
+             Required = new[] { "addresses" }
+         },
+         new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }));
+
+    [Test]
+    [AsyncOnly]
+    public async Task AutoFunctionCallingWithStreamingExampleAsync()
+    {
+        #region Snippet:FunctionsWithStreaming_CreateClient
+#if SNIPPET
+        var connectionString = System.Environment.GetEnvironmentVariable("PROJECT_CONNECTION_STRING");
+        var modelDeploymentName = System.Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME");
+#else
+        var connectionString = TestEnvironment.AzureAICONNECTIONSTRING;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+#endif
+        AgentsClient client = new(connectionString, new DefaultAzureCredential());
+        #endregion
+
+        #region Snippet:FunctionsWithStreamingUpdateHandling
+        #endregion
+        #region Snippet:FunctionsWithStreaming_CreateAgent
+        Agent agent = await client.CreateAgentAsync(
+            model: modelDeploymentName,
+            name: "SDK Test Agent - Functions",
+                instructions: "You are a weather bot. Use the provided functions to help answer questions. "
+                    + "Customize your responses to the user's preferences as much as possible and use friendly "
+                    + "nicknames for cities whenever possible.",
+            tools: [geHhumidityByAddressTool, getWeatherByAddressesTool]
+        );
+        #endregion
+        #region Snippet:FunctionsWithStreaming_CreateThread
+        AgentThread thread = await client.CreateThreadAsync();
+
+        ThreadMessage message = await client.CreateMessageAsync(
+            thread.Id,
+            MessageRole.User,
+            "Get weather and humidity for street, 123 1st St in city, Seattle and street, 456 2nd Ave in city, Bellevue");
+        #endregion
+        #region Snippet:FunctionsWithStreamingUpdateCycle
+        List<ToolOutput> toolOutputs = new();
+        Dictionary<string, Delegate> delegates = new();
+        delegates.Add(nameof(GetWeatherByAddresses), GetWeatherByAddresses);
+        delegates.Add(nameof(GetHumidityByAddress), GetHumidityByAddress);
+        client.EnableAutoFunctionCalls(delegates);
+        await foreach (StreamingUpdate streamingUpdate in client.CreateRunStreamingAsync(thread.Id, agent.Id))
+        {
+            if (streamingUpdate.UpdateKind == StreamingUpdateReason.RunCreated)
+            {
+                Console.WriteLine("--- Run started! ---");
+            }
+            else if (streamingUpdate is MessageContentUpdate contentUpdate)
+            {
+                Console.Write(contentUpdate.Text);
+            }
+            else if (streamingUpdate.UpdateKind == StreamingUpdateReason.RunCompleted)
+            {
+                Console.WriteLine();
+                Console.WriteLine("--- Run completed! ---");
+            }
+        }
+
+        #endregion
+        #region Snippet:FunctionsWithStreaming_Cleanup
+        await client.DeleteThreadAsync(thread.Id);
+        await client.DeleteAgentAsync(agent.Id);
+        #endregion
+    }
+
+    [Test]
+    [SyncOnly]
+    public void AutoFunctionCallingWithStreamingExample()
+    {
+#if SNIPPET
+        var connectionString = System.Environment.GetEnvironmentVariable("PROJECT_CONNECTION_STRING");
+        var modelDeploymentName = System.Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME");
+#else
+        var connectionString = TestEnvironment.AzureAICONNECTIONSTRING;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+#endif
+        AgentsClient client = new(connectionString, new DefaultAzureCredential());
+
+        #region Snippet:FunctionsWithStreamingSync_CreateAgent
+        Agent agent = client.CreateAgent(
+            model: modelDeploymentName,
+            name: "SDK Test Agent - Functions",
+                instructions: "You are a weather bot. Use the provided functions to help answer questions. "
+                    + "Customize your responses to the user's preferences as much as possible and use friendly "
+                    + "nicknames for cities whenever possible.",
+            tools: [getWeatherByAddressesTool, geHhumidityByAddressTool]
+        );
+        #endregion
+        #region Snippet:FunctionsWithStreamingSync_CreateThread
+        AgentThread thread = client.CreateThread();
+
+        ThreadMessage message = client.CreateMessage(
+            thread.Id,
+            MessageRole.User,
+            "Get weather and humidity for street, 123 1st St in city, Seattle and street, 456 2nd Ave in city, Bellevue");
+        #endregion
+        #region Snippet:FunctionsWithStreamingSyncUpdateCycle
+        List<ToolOutput> toolOutputs = [];
+        Dictionary<string, Delegate> delegates = new();
+        delegates.Add(nameof(GetWeatherByAddresses), GetWeatherByAddresses);
+        delegates.Add(nameof(GetHumidityByAddress), GetHumidityByAddress);
+        client.EnableAutoFunctionCalls(delegates);
+        CollectionResult<StreamingUpdate> stream = client.CreateRunStreaming(thread.Id, agent.Id);
+        foreach (StreamingUpdate streamingUpdate in stream)
+        {
+            if (streamingUpdate.UpdateKind == StreamingUpdateReason.RunCreated)
+            {
+                Console.WriteLine("--- Run started! ---");
+            }
+            else if (streamingUpdate is MessageContentUpdate contentUpdate)
+            {
+                Console.Write(contentUpdate.Text);
+            }
+            else if (streamingUpdate.UpdateKind == StreamingUpdateReason.RunCompleted)
+            {
+                Console.WriteLine();
+                Console.WriteLine("--- Run completed! ---");
+            }
+        }
+        #endregion
+        #region Snippet:FunctionsWithStreamingSync_Cleanup
+        client.DeleteThread(thread.Id);
+        client.DeleteAgent(agent.Id);
+        #endregion
+    }
+
     [Test]
     [AsyncOnly]
     public async Task FunctionCallingWithStreamingExampleAsync()
